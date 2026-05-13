4962ce642f5e685ab4192c4d716cb3db32432337
Ceze Darling <cezedarling@czgrafx.com>
2026-05-13 13:16:45 -0500

    Upload Init

---
+++ capacitor-game-connect-2026-main/ios/Plugin/CapacitorGameConnectPlugin.swift
@@ -0,0 +1,45 @@
+import Foundation
+import Capacitor
+import GameKit
+
+/**
+ * Please read the Capacitor iOS Plugin Development Guide
+ * here: https://capacitorjs.com/docs/plugins/ios
+ */
+
+@objc(CapacitorGameConnectPlugin)
+public class CapacitorGameConnectPlugin: CAPPlugin {
+    private let implementation = CapacitorGameConnect()
+
+    @objc func signIn(_ call: CAPPluginCall) {
+        implementation.signIn(call, (self.bridge?.viewController)!)
+    }
+    
+    @objc func showLeaderboard(_ call: CAPPluginCall) {
+        implementation.showLeaderboard(call, (self.bridge?.viewController)!)
+    }
+    
+    @objc func showAllLeaderboards(_ call: CAPPluginCall) {
+        implementation.showAllLeaderboards(call, (self.bridge?.viewController)!)
+    }
+
+    @objc func showAchievements(_ call: CAPPluginCall) {
+        implementation.showAchievements(call, (self.bridge?.viewController)!)
+    }
+    
+    @objc func submitScore(_ call: CAPPluginCall) {
+        implementation.submitScore(call)
+    }
+    
+    @objc func unlockAchievement(_ call: CAPPluginCall) {
+        implementation.unlockAchievement(call)
+    }
+    
+    @objc func incrementAchievementProgress(_ call: CAPPluginCall) {
+        implementation.incrementAchievementProgress(call)
+    }
+    
+    @objc func getUserTotalScore(_ call: CAPPluginCall) {
+        implementation.getUserTotalScore(call)
+    }
+}
